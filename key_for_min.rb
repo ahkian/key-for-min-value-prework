a2733af76281c761d099cff5717958ef7ab67e23
Daniel Brazel <dbrazel4@gmail.com>
2018-03-18 17:02:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,8 @@ def key_for_min_value(name_hash)
   value_array = []
   name_hash.each do |k, v|
     value_array << v
+  end
+  name_hash.each do |k, v|
     first_value = value_array[0]
     if v < first_value
       first_value = v
